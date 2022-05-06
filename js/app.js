b9b283c40031e09ec52557f649c505768fc54313
MarcoVitone <83236213+MarcoVitone@users.noreply.github.com>
2022-05-06 20:01:11 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -33,91 +33,88 @@ const handleTrailers = async () => {
 handleTrailers();
 
 const inTheatreHandler = async () => {
-    try {
-        const respose = await axios.get(
-          "https://imdb-api.com/en/API/InTheaters/k_iqskuj22"
-        );
-        const data = await respose.data;
-        console.log(data)
-        data.items.map(value => {
-            const movie = document.getElementById("pippo");
-             const card = document.createElement("div");
-             card.className = "container-main-explore-cinema-movie-card";
-             // Create Image poster
-             const cardImg = document.createElement("div");
-             cardImg.className = "container-main-explore-cinema-movie-card-img";
-             const img = document.createElement("img");
-             movie.appendChild(card);
-             card.appendChild(cardImg);
-             cardImg.appendChild(img);
-             img.src = value.image;
-             const bookmark = document.createElement("i");
-             bookmark.className = "fa-solid fa-bookmark";
-             const plus = document.createElement("i");
-             plus.className = "fa-solid fa-plus";
-             bookmark.appendChild(plus);
-             cardImg.appendChild(bookmark);
-             //create description part
-             const description = document.createElement("div");
-             description.className =
-               "container-main-explore-cinema-movie-card-description";
-             card.appendChild(description);
-             //create rating
-             const rating = document.createElement("div");
-             rating.className =
-               "container-main-explore-cinema-movie-card-description-rating";
-             description.appendChild(rating);
-             const spanRating = document.createElement("span");
-             const solidStar = document.createElement("i");
-             solidStar.className = "fa-solid fa-star";
-             spanRating.appendChild(solidStar);
-             const ratingVote = document.createElement("p");
-             ratingVote.innerHTML = "6.5";
-             spanRating.appendChild(ratingVote);
-             rating.appendChild(spanRating);
-             const starButton = document.createElement("button");
-             const regularStar = document.createElement("i");
-             regularStar.className = "fa-regular fa-star";
-             starButton.appendChild(regularStar);
-             rating.appendChild(starButton);
-             // create film title
-             const title = document.createElement("div");
-             title.className =
-               "container-main-explore-cinema-movie-card-description-title";
-             description.appendChild(title);
-             const filmTitle = document.createElement("h3");
-             filmTitle.innerHTML = value.title;
-             title.appendChild(filmTitle);
-             //create cinema ticket
-             const cinema = document.createElement("div");
-             cinema.className =
-               "container-main-explore-cinema-movie-card-description-cinema";
-             description.appendChild(cinema);
-             const cinemaLink = document.createElement("a");
-             cinema.appendChild(cinemaLink);
-             const ticket = document.createElement("i");
-             ticket.className = "fa-solid fa-ticket-simple";
-             cinemaLink.appendChild(ticket);
-             cinemaLink.appendChild(
-               document.createTextNode(" Orari degli spett...")
-             );
-             const cinemaTrailer = document.createElement("div");
-             cinemaTrailer.className =
-               "container-main-explore-cinema-movie-card-description-trailer";
-             description.appendChild(cinemaTrailer);
-             const trailerLink = document.createElement("a");
-             cinemaTrailer.appendChild(trailerLink);
-             const play = document.createElement("i");
-             play.className = "fa-solid fa-play";
-             trailerLink.appendChild(play);
+  try {
+    const respose = await axios.get(
+      "https://imdb-api.com/en/API/InTheaters/k_iqskuj22"
+    );
+    const data = await respose.data;
+    console.log(data);
+    data.items.map((value) => {
+      const movie = document.getElementById("pippo");
+      const card = document.createElement("div");
+      card.className = "container-main-explore-cinema-movie-card";
+      // Create Image poster
+      const cardImg = document.createElement("div");
+      cardImg.className = "container-main-explore-cinema-movie-card-img";
+      const img = document.createElement("img");
+      movie.appendChild(card);
+      card.appendChild(cardImg);
+      cardImg.appendChild(img);
+      img.src = value.image;
+      const bookmark = document.createElement("i");
+      bookmark.className = "fa-solid fa-bookmark";
+      const plus = document.createElement("i");
+      plus.className = "fa-solid fa-plus";
+      bookmark.appendChild(plus);
+      cardImg.appendChild(bookmark);
+      //create description part
+      const description = document.createElement("div");
+      description.className =
+        "container-main-explore-cinema-movie-card-description";
+      card.appendChild(description);
+      //create rating
+      const rating = document.createElement("div");
+      rating.className =
+        "container-main-explore-cinema-movie-card-description-rating";
+      description.appendChild(rating);
+      const spanRating = document.createElement("span");
+      const solidStar = document.createElement("i");
+      solidStar.className = "fa-solid fa-star";
+      spanRating.appendChild(solidStar);
+      const ratingVote = document.createElement("p");
+      ratingVote.innerHTML = "6.5";
+      spanRating.appendChild(ratingVote);
+      rating.appendChild(spanRating);
+      const starButton = document.createElement("button");
+      const regularStar = document.createElement("i");
+      regularStar.className = "fa-regular fa-star";
+      starButton.appendChild(regularStar);
+      rating.appendChild(starButton);
+      // create film title
+      const title = document.createElement("div");
+      title.className =
+        "container-main-explore-cinema-movie-card-description-title";
+      description.appendChild(title);
+      const filmTitle = document.createElement("h3");
+      filmTitle.innerHTML = value.title;
+      title.appendChild(filmTitle);
+      //create cinema ticket
+      const cinema = document.createElement("div");
+      cinema.className =
+        "container-main-explore-cinema-movie-card-description-cinema";
+      description.appendChild(cinema);
+      const cinemaLink = document.createElement("a");
+      cinema.appendChild(cinemaLink);
+      const ticket = document.createElement("i");
+      ticket.className = "fa-solid fa-ticket-simple";
+      cinemaLink.appendChild(ticket);
+      cinemaLink.appendChild(document.createTextNode(" Orari degli spett..."));
+      const cinemaTrailer = document.createElement("div");
+      cinemaTrailer.className =
+        "container-main-explore-cinema-movie-card-description-trailer";
+      description.appendChild(cinemaTrailer);
+      const trailerLink = document.createElement("a");
+      cinemaTrailer.appendChild(trailerLink);
+      const play = document.createElement("i");
+      play.className = "fa-solid fa-play";
+      trailerLink.appendChild(play);
 
-             return;
-        })
-
-    } catch (error) {
-        console.log(error)
-    }
-}
+      return;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 inTheatreHandler();
 
 const boxOffice = async () => {
@@ -142,7 +139,7 @@ const boxOffice = async () => {
       const add = document.createElement("div");
       add.className =
         "container-main-explore-bestBoxOffice-movies-left-movie-add";
-    movie.appendChild(add);
+      movie.appendChild(add);
       const bookmark = document.createElement("i");
       bookmark.className = "fa-solid fa-bookmark";
       const plus = document.createElement("i");
@@ -150,23 +147,23 @@ const boxOffice = async () => {
       bookmark.appendChild(plus);
       add.appendChild(bookmark);
       const title = document.createElement("div");
-      movie.appendChild(title)
+      movie.appendChild(title);
       title.className =
         "container-main-explore-bestBoxOffice-movies-left-movie-title";
-        const movieTitle = document.createElement("h3");
-        movieTitle.innerHTML = data[i].title;
-        title.appendChild(movieTitle);
-        const money = document.createElement("p");
-        money.innerHTML = data[i].weekend;
-        title.appendChild(money);
-        const ticket = document.createElement("div");
-        ticket.className =
-          "container-main-explore-bestBoxOffice-movies-left-movie-ticket";
-          movie.appendChild(ticket);
-          const ticketFont = document.createElement("i");
-          ticketFont.className = "fa-solid fa-ticket-simple";
-          ticket.appendChild(ticketFont);
-    };
+      const movieTitle = document.createElement("h3");
+      movieTitle.innerHTML = data[i].title;
+      title.appendChild(movieTitle);
+      const money = document.createElement("p");
+      money.innerHTML = data[i].weekend;
+      title.appendChild(money);
+      const ticket = document.createElement("div");
+      ticket.className =
+        "container-main-explore-bestBoxOffice-movies-left-movie-ticket";
+      movie.appendChild(ticket);
+      const ticketFont = document.createElement("i");
+      ticketFont.className = "fa-solid fa-ticket-simple";
+      ticket.appendChild(ticketFont);
+    }
 
     const right = document.getElementById("right");
 
@@ -207,8 +204,7 @@ const boxOffice = async () => {
       const ticketFont = document.createElement("i");
       ticketFont.className = "fa-solid fa-ticket-simple";
       ticket.appendChild(ticketFont);
-    };
-
+    }
   } catch (error) {
     console.log(error);
   }
@@ -217,37 +213,36 @@ const boxOffice = async () => {
 boxOffice();
 
 const comingSoon = async () => {
-    const response = await axios.get(
-      "https://imdb-api.com/en/API/ComingSoon/k_iqskuj22"
-    );
-    const data = response.data.items;
+  const response = await axios.get(
+    "https://imdb-api.com/en/API/ComingSoon/k_iqskuj22"
+  );
+  const data = response.data.items;
 
-    
-    document.getElementById("date-1").innerHTML = data[0].releaseState;
-    document.getElementById("date-2").innerHTML = data[1].releaseState;
-    document.getElementById("date-3").innerHTML = data[2].releaseState;
-    document.getElementById("title-1").innerHTML = data[0].title;
-    document.getElementById("title-2").innerHTML = data[1].title;
-    document.getElementById("title-3").innerHTML = data[2].title;
+  document.getElementById("date-1").innerHTML = data[0].releaseState;
+  document.getElementById("date-2").innerHTML = data[1].releaseState;
+  document.getElementById("date-3").innerHTML = data[2].releaseState;
+  document.getElementById("title-1").innerHTML = data[0].title;
+  document.getElementById("title-2").innerHTML = data[1].title;
+  document.getElementById("title-3").innerHTML = data[2].title;
 
-    const ids = [data[0].id, data[1].id, data[2].id]
-    const image = async () => {
-        try {
-           for(let i = 0; i < ids.length; i++){
-             const response = await axios.get(`https://imdb-api.com/en/API/Trailer/k_iqskuj22/${ids[i]}`);
-            const data = await response.data;
-            document.getElementById(`img-${i + 1}`).src = data.thumbnailUrl;
-            document.getElementById(`img-${i + 1}`).src = data.thumbnailUrl;
-            document.getElementById(`img-${i + 1}`).src = data.thumbnailUrl;
-            console.log(data)  
-           }
-            
-        
-        } catch (error) {
-           console.log(error) 
-        }  
+  const ids = [data[0].id, data[1].id, data[2].id];
+  const image = async () => {
+    try {
+      for (let i = 0; i < ids.length; i++) {
+        const response = await axios.get(
+          `https://imdb-api.com/en/API/Trailer/k_iqskuj22/${ids[i]}`
+        );
+        const data = await response.data;
+        document.getElementById(`img-${i + 1}`).src = data.thumbnailUrl;
+        document.getElementById(`img-${i + 1}`).src = data.thumbnailUrl;
+        document.getElementById(`img-${i + 1}`).src = data.thumbnailUrl;
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
     }
-    image();
-}
+  };
+  image();
+};
 
 comingSoon();
